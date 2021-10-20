0b2ee7369b066d8f27342b9c0143e0641a579ce2
Albert Navasardyan <albertnavasardyan@gmail.com>
2021-10-20 18:22:13 +0400

    remove logs

--- services/crio/lambda.js
+++ services/crio/lambda.js
@@ -2,17 +2,11 @@ const { ApolloServer } = require('apollo-server-lambda');
 const logs = require('@tidepoollabs/node-lambda-logs');
 const { getUserFromRequest } = require('@tidepoollabs/node-auth');
 
-console.log(11111111);
-
 const schema = require('./schema');
 const models = require('./models');
 const loaders = require('./loaders');
 
-console.log(22222222);
-
 logs.init(process.env.SENTRY_DSN);
-
-console.log(33333333);
 
 const server = new ApolloServer({
   schema,
@@ -32,15 +26,11 @@ const server = new ApolloServer({
   },
 });
 
-console.log(4444444444);
-
 let invokedCount = 0;
 exports.handler = logs.wrapHandler(
   async function (event, context) {
-    console.log(55555555);
     console.log('INVOCATION NUMBER:', invokedCount + 1);
     console.log('EVENT: \n' + JSON.stringify(event, null, 2));
-    console.log(66666666);
     if (invokedCount) {
       // restart connection pool to ensure connections are not re-used across invocations
       models.sequelize.connectionManager.initPools();
@@ -50,7 +40,6 @@ exports.handler = logs.wrapHandler(
         delete models.sequelize.connectionManager.getConnection;
       }
     }
-    console.log(77777777);
     try {
       return await server.createHandler()(event, context);
     } finally {
