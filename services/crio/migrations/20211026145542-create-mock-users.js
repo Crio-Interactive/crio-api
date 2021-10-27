92c6b12e24090e6897d59f2cc6b40c85c597e025
Narine <narine@tidepoollabs.com>
2021-10-27 08:50:23 +0400

    fix(User): Fixed table creating issue

--- services/crio/migrations/20211026145542-create-mock-users.js
+++ services/crio/migrations/20211026145542-create-mock-users.js
@@ -31,7 +31,48 @@ const mockUsers = [
 ];
 
 module.exports = {
-  up: async () => {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      fbUserId: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+    });
     await models.User.bulkCreate(mockUsers.map(item => ({
       ...item,
       userId: uuidv4(),
