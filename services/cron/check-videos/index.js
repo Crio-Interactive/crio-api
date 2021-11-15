65203d803d7073cf65573958cfb0b5815e1c2ba3
VanTigranyan <lostfank@gmail.com>
2021-11-15 21:24:10 +0400

    feat(App): Fix model usage in cron jobs

---
+++ services/cron/check-videos/index.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const dotenv = require('dotenv');
+const { Op } = require('sequelize');
+const { vimeoClient } = require('../config/httpClient');
+const db = require('../models');
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+const handler = async () => {
+  try {
+    const { Artwork } = db;
+    const videosToCheck = await Artwork.findAll({
+      where: {
+        status: {
+          [Op.not]: 'available',
+        }
+      },
+      limit: 10,
+    });
+    if (videosToCheck.length) {
+      const requests = videosToCheck.map((vid) => vimeoClient.get(vid.videoUri));
+      const results = await Promise.all(requests);
+      const available = results.filter(res => res.data.status.available).map(itm => itm.data.uri);
+      if (available.length) {
+        await Artwork.update({
+          status: 'available',
+        }, {
+          where: {
+            videoUri: available,
+          },
+        });
+      }
+    }
+    console.log('Successfully updated video statuses');
+  } catch (e) {
+    console.error('Error checking videos statuses', e);
+  }
+};
+
+module.exports = handler;
--- services/cron/check-videos/lambda.js
+++ services/cron/check-videos/lambda.js
@@ -1,40 +1,7 @@
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const logs = require('@tidepoollabs/node-lambda-logs');
-const sequelize = require('sequelize');
-const { vimeoClient } = require('../config/httpClient');
+const handler = require('./index');
 
 logs.init(process.env.SENTRY_DSN);
 
-const handler = async () => {
-  try {
-    console.log('models', sequelize.models);
-    const { Artwork } = sequelize.models;
-    const videosToCheck = await Artwork.findAll({
-      where: {
-        status: {
-          [sequelize.Op.not]: 'available',
-        }
-      },
-      limit: 10,
-    });
-    if (videosToCheck.length) {
-      const requests = videosToCheck.map((vid) => vimeoClient.get(vid.videoUri));
-      const results = await Promise.all(requests);
-      const available = results.filter(res => res.data.status.available).map(itm => itm.data.uri);
-      if (available.length) {
-        await Artwork.update({
-          status: 'available',
-        }, {
-          where: {
-            videoUri: available,
-          },
-        });
-      }
-    }
-    console.log('Successfully updated video statuses');
-  } catch (e) {
-    console.error('Error checking videos statuses', e);
-  }
-};
+
 exports.handler = logs.wrapHandler(handler);
