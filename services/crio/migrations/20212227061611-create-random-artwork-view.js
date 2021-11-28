4d68bc4c2d069f19afc3230b4bdc4d1c2a2f2707
Narine <narine@tidepoollabs.com>
2021-11-28 23:34:38 +0400

    feat(Following): Added get followings query

--- services/crio/migrations/20212227061611-create-random-artwork-view.js
+++ services/crio/migrations/20212227061611-create-random-artwork-view.js
@@ -2,22 +2,26 @@
 
 module.exports = {
   up: (queryInterface) => queryInterface.sequelize.query(`
-  CREATE VIEW "RandomArtworks" AS
-  SELECT
-         row_number() OVER () AS id,
-         CASE WHEN 'name'= ANY("visibility") THEN CONCAT("firstName", ' ', "lastName")
-              WHEN 'username'= ANY("visibility") THEN "username"
-              ELSE "Users"."email" END
-         AS name,
-         "Artworks"."userId",
-         "fbUserId",
-         "videoUri",
-         "thumbnailUri",
-         "title",
-         "description"
-  FROM "Users" INNER JOIN "Artworks" ON "Users".id = "Artworks"."userId"
-  WHERE status='available'
-
+    CREATE VIEW "RandomArtworks" AS
+    SELECT
+      row_number() OVER () AS id,
+      CASE WHEN 'name'= ANY("visibility") THEN CONCAT("firstName", ' ', "lastName")
+           WHEN 'username'= ANY("visibility") THEN "username"
+           ELSE "Users"."email" END
+      AS name,
+      "firstName",
+      "lastName",
+      "username",
+      "email",
+      "visibility",
+      "Artworks"."userId",
+      "fbUserId",
+      "videoUri",
+      "thumbnailUri",
+      "title",
+      "description"
+    FROM "Users" INNER JOIN "Artworks" ON "Users".id = "Artworks"."userId"
+    WHERE status='available' AND "Users"."deletedAt" IS NULL AND "Artworks"."deletedAt" IS NULL
   `),
 
   down: queryInterface => queryInterface.sequelize.query('DROP VIEW IF EXISTS "RandomArtworks"'),
