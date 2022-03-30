845e1496332205d0e96c72c7ad95684baf06ff35
davitmuradyan <davitmuradyan5@gmail.com>
2022-03-30 17:31:27 +0400

    change domain and some envs

--- .circleci/env-setup.sh
+++ .circleci/env-setup.sh
@@ -3,38 +3,38 @@
 {
   echo "export STACK_ACCOUNT=$(aws sts get-caller-identity --query "Account" --output text)"
   echo "export NODE_ENV=production"
-  echo "export DOMAIN_NAME=criointeractive.com"
+  echo "export DOMAIN_NAME=crio.live"
 } >> "$BASH_ENV"
 
 if [ "${CIRCLE_BRANCH}" == "develop" ]
 then
   {
-    echo "export CLIENT_URL=https://crio-qa.criointeractive.com/"
+    echo "export CLIENT_URL=https://crio-qa.crio.live/"
     echo "export SUBDOMAIN_NAME=crio-qa-api"
     echo "export STACK_REGION=us-west-2"
     echo "export APP_NAME=crio-development"
-    echo "export SENDGRID_VERIFIED_SENDER=info@criointeractive.com"
+    echo "export SENDGRID_VERIFIED_SENDER=info@crio.live"
     echo "export SENDGRID_CC_EMAILS=narine@tidepoollabs.com"
   } >> "$BASH_ENV"
 elif [ "${CIRCLE_BRANCH}" == "staging" ]
 then
   {
-    echo "export CLIENT_URL=https://crio-staging.criointeractive.com/"
+    echo "export CLIENT_URL=https://crio-staging.crio.live/"
     echo "export SUBDOMAIN_NAME=crio-staging-api"
     echo "export STACK_REGION=us-west-2"
-    echo "export APP_NAME=crio-staging"
-    echo "export SENDGRID_VERIFIED_SENDER=info@criointeractive.com"
+    echo "export APP_NAME=crio-in-staging"
+    echo "export SENDGRID_VERIFIED_SENDER=info@crio.live"
     echo "export SENDGRID_CC_EMAILS=narine@tidepoollabs.com"
   } >> "$BASH_ENV"
 elif [ "${CIRCLE_BRANCH}" == "master" ]
 then
   {
-    echo "export CLIENT_URL=https://www.criointeractive.com/"
+    echo "export CLIENT_URL=https://www.crio.live/"
     echo "export SUBDOMAIN_NAME=api"
     echo "export STACK_REGION=us-east-1"
-    echo "export APP_NAME=crio-production"
-    echo "export SENDGRID_VERIFIED_SENDER=info@criointeractive.com"
-    echo "export SENDGRID_CC_EMAILS=klodi.beqiri@criointeractive.com"
+    echo "export APP_NAME=crio-in-production"
+    echo "export SENDGRID_VERIFIED_SENDER=info@crio.live"
+    echo "export SENDGRID_CC_EMAILS=klodi.beqiri@crio.live"
   } >> "$BASH_ENV"
 else
   #should not be executed
