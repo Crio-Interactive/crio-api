71b6a7d60c0cb5ad591b2e72d02254e4a8a3a7d5
Narine <narine@tidepoollabs.com>
2022-06-03 13:12:48 +0400

    fix(Product): Fix send mail

--- services/crio/config/mail.js
+++ services/crio/config/mail.js
@@ -1,17 +1,16 @@
 const sgMail = require('@sendgrid/mail');
 
-const { SENDGRID_API_KEY, SENDGRID_VERIFIED_SENDER, SENDGRID_CC_EMAILS } = require('./environment');
+const { SENDGRID_API_KEY, SENDGRID_CC_EMAILS, SENDGRID_VERIFIED_SENDER } = require('./environment');
 
 sgMail.setApiKey(SENDGRID_API_KEY);
 
-const sendMail = async ({ to, subject, text, cc }) => {
-  return sgMail.send({
+const sendMail = async ({ to, subject, text, cc }) =>
+  sgMail.send({
     to,
     from: SENDGRID_VERIFIED_SENDER,
     subject,
     text,
-    cc: [SENDGRID_CC_EMAILS, cc].filter(email => (email !== to)),
+    cc: [SENDGRID_CC_EMAILS, cc].filter(email => email !== to),
   });
-};
 
 module.exports = sendMail;
