bc2c957430b93d4381709f9d87e63decba62addc
davitmuradyan <davitmuradyan5@gmail.com>
2022-08-29 13:22:59 +0400

    fix: comment bootstrap

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ set -o errexit
 set -o xtrace
 
 echo "deploying backend"
-npx cdk bootstrap aws://"$STACK_ACCOUNT"/"$STACK_REGION"
+# npx cdk bootstrap aws://"$STACK_ACCOUNT"/"$STACK_REGION"
 npx cdk deploy --all --require-approval=never
