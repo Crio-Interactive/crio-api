404c4c56fe05e284eaef203e9060d29f11598c36
Narine <narine@tidepoollabs.com>
2022-08-30 13:16:20 +0400

    chore(Earn More): Add test logs

--- services/crio/resolvers/user.resolvers.js
+++ services/crio/resolvers/user.resolvers.js
@@ -278,7 +278,9 @@ module.exports = {
         if (exist) {
           throw new Error('The invitation has already been accepted');
         }
+        console.log(email, 'EMAIL-EMAIL');
         const invitation = await models.Invitation.findOne({ where: { email } });
+        console.log(invitation, 'INVITATION-INVITATION');
         if (invitation) {
           await models.Creator.create({ email });
         } else {
